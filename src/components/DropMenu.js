944d565f8a8d20e6586885264951e69dff677beb
Gulshan196 <109276430+Gulshan196@users.noreply.github.com>
2022-12-17 17:50:10 +0530

    complete

---
+++ src/components/DropMenu.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react'
+
+const subjects = [
+    'science','commerce','arts'
+];
+
+  const science = ['maths','physics','chemistry'];
+ const commerce = ['accounts','bussiness','maths'];
+const arts = ['humanities','english','political science'];
+
+// const science_num = [10,30,50];
+// const arts_num = [90,40,20];
+// const commerce_num = [90,80,50];
+const DropMenu = () => {
+    const [sub,setSub] = useState(null);
+    const [show ,setShow] = useState(false);
+    const [num,setNum] = useState([]);
+
+    // console.log(sub
+
+    useEffect(()=>{
+
+    },[num])
+
+    function handleClick() {
+        setShow((show)=>!show)
+    }
+
+  return (
+    <div>
+       <div className="dropdown" > <button onClick={handleClick}>subjects</button>
+
+  {subjects && subjects.map((el)=>{
+   return( <div className={!show ? "dropdown-content":""} key = {el} value={el} onClick ={(e)=>setSub(el)}>{el}</div>)
+  })}
+
+  
+</div>
+
+<div className='subs-info'>
+
+  {sub && sub === 'science' && science.map((el)=>{
+    return( <div className = "" key = {el} value={el} onClick = {()=>setNum([20,40,30])}>{el}</div>)
+  })}
+
+{sub && sub === 'commerce' && commerce.map((el)=>{
+    return( <div className = "" key = {el} value={el} onClick = {()=>setNum([60,40,10])} >{el}</div>)
+  })}
+
+{sub && sub === 'arts' && arts.map((el)=>{
+    return( <div className = "" key = {el} value={el} onClick = {()=>setNum([10,90,50])} >{el}</div>)
+  })}
+
+
+</div>
+
+<div className='num-info'>
+{num  && num.map((el)=>{
+    return( <div className = "" key = {el} value={el}  >{el}</div>)
+  })}
+  
+    </div> 
+
+
+
+
+    </div>
+  )
+}
+
+export default DropMenu
